3b28349c1ed0ce91c5b5e42fe76fb326fcf22c41
Gaurav Kanwal <kanwalgk25@gmail.com>
2026-02-25 20:57:16 +0530

    Add route imports and declarations for dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos; Update multer middleware to include file filtering

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,30 @@
 import multer from "multer";
-
+import path from 'path';
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
         cb(null,'./public/temp')
     },
     filename:function(req,file,cb){
-        cb(null,file.originalname)
+        const uniqueSuffix=Date.now()+'-'+Math.round(Math.random()*1E9)
+        cb(null,uniqueSuffix+path.extname(file.originalname))
     }
 })
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype.startsWith("image/") ||
+    file.mimetype.startsWith("video/")
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images and videos are allowed"), false);
+  }
+};
+
 export const upload=multer(
     {
-        storage:storage
+        storage,
+        fileFilter,
     }
 )
